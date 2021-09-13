45bb128ac59eec18c503eb66282464b96f98dd50
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-09-13 01:26:51 -0300

    feat: create user ViewController

---
+++ LMS/User/Http/Controllers/ViewController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LMS\User\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class ViewController extends Controller
+{
+    public function viewProfile () : View
+    {
+        $user = Auth::user();
+        return view('user::profile', compact('user'));
+    }
+}
