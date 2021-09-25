98ccf59c66fbf24271d1888bdafdfbefdd032176
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-09-25 14:14:43 -0300

    feat: add botão para a tela de edição

--- LMS/User/Resources/views/profile.blade.php
+++ LMS/User/Resources/views/profile.blade.php
@@ -12,11 +12,11 @@
         <div class="col-10">
             <div class="card">
                 <div class="row justify-content-around mt-2">
-                    <div class="col-4 d-flex justify-content-center align-items-center">
+                    <div class="col-5 d-flex justify-content-center align-items-center">
                         <img src="https://placehold.it/300x300" class="photo-profile">
                     </div>
                     <div class="col-7">
-                        <h3>{{ $user->name }}</h3>
+                        <h3>{{ $user->username }}</h3>
                         <p>{{ $user->email }}</p>
                         <p>Membro desde: {{  $user['created_at']->format('d/m/Y H:i') }}</p>
                         <p>Último acesso: {{ $user->last_seen }}</p>
@@ -25,6 +25,11 @@
                         <p>Cidade: {{$user->address->getCityAndState()}}</p>
                         <p>Endereço: {{$user->address->getFullAddress()}}</p>
                     </div>
+                </div>
+                <div class="row justify-content-center">
+                    <a href="{{ route('edit-profile') }}">
+                        <button class="btn btn-primary"> Editar Perfil</button>
+                    </a>
                 </div>
             </div>
         </div>
